1d307be325c18d7662f8ac07fa88a6b1b2f6af03
Mohammad Farwan <85956258+MohammadFarwan@users.noreply.github.com>
2024-07-12 10:27:33 +0300

    Time: 119 ms (17.47%) | Memory: 48.4 MB (66.87%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] NumberGame(int[] nums) {
+        Array.Sort(nums);
+        for(int i=0; i<=nums.Length-2; i+=2)
+        {
+            int temp = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1]= temp;
+        }
+        return nums;
+    }
+}

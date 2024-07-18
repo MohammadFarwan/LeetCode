0bebff6640f34dda55e66599cf08437d49af8cb2
Mohammad Farwan <85956258+MohammadFarwan@users.noreply.github.com>
2024-07-18 07:09:07 +0300

    Time: 155 ms (89.81%) | Memory: 81.7 MB (54.78%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public string[] SortPeople(string[] names, int[] heights) 
+    {
+        Array.Sort(heights, names); 
+        var name = names.ToList<string>();
+        name.Reverse();
+        return name.ToArray();
+    }
+}

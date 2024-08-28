d501712b2ed1631ded88c4f210246fd3c5b1bb92
Mohammad Farwan <85956258+MohammadFarwan@users.noreply.github.com>
2024-08-28 16:26:13 +0300

    Time: 271 ms (42.72%) | Memory: 55.4 MB (91.04%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -0,0 +1,9 @@
+public class Solution 
+{
+    public bool ContainsDuplicate(int[] nums) 
+    {
+        HashSet<int> set = new HashSet<int>(nums);
+        
+        return nums.Length != set.Count;
+    }
+}
